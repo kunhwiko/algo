bc80a0d27b655f7646b18555f728db630065b5eb
Kun Hwi Ko <kokunhwi@gmail.com>
2021-01-19 14:49:23 -0500

    fix tab/space

--- 6-others/sliding_windows.py
+++ 6-others/sliding_windows.py
@@ -1,27 +1,25 @@
 # Sliding Window "slides a window" of size k through an array from left to right
 
-# Example 1:
+# Example 1
 # Find the max sum of a size k subarray 
 
 def max_subarray(nums, k):
     curr = best = sum(nums[:k])
     for i in range(1, len(nums)-k+1):
-	    curr -= nums[i-1]
-	    curr += nums[i+k-1]
-	    best = max(best, curr)
+        curr -= nums[i-1]
+        curr += nums[i+k-1]
+        best = max(best, curr)
     return best 
 
-
-# Example 2:
+# Example 2
 # Find the longest substring without repeating characters 
 
 def longest_nonrepeat_substring(s):
     indices = {}
     start = longest = 0
     for i in range(len(s)):
-	    if s[i] in indices and start <= indices[s[i]]:
-		    start = indices[s[i]] + 1
-	    indices[s[i]] = i
-	    longest = max(longest, i-start+1)
+        if s[i] in indices and start <= indices[s[i]]:
+            start = indices[s[i]] + 1
+        indices[s[i]] = i 
+        longest = max(longest, i-start+1)
     return longest 
-
