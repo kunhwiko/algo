f9578b76a0a7c516f9440358b874c7ec42bceb14
Kun Hwi Ko <kokunhwi@gmail.com>
2021-01-21 21:49:12 -0500

    update backtracking

--- 6-others/backtracking.py
+++ 6-others/backtracking.py
@@ -16,3 +16,18 @@ class Example1:
             return 
         self.backtrack(curr + "(", l-1, r)
         self.backtrack(curr + ")", l, r-1)
+
+# Example 2
+# Find all permutations of an element 
+
+class Example2:
+    def permutations(self, nums):
+        self.res = []
+        self.backtrack([], nums)
+        return self.res 
+    
+    def backtrack(self, curr, nums):
+        if not nums:
+            self.res.append(curr)
+        for i in range(len(nums)):
+            self.backtrack(curr+[nums[i]], nums[:i] + nums[i+1:])
